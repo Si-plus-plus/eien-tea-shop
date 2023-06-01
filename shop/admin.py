039cd27e64d14280f6388e80e916abc6ac4b20fa
Si-plus-plus <siplusplus2019@gmail.com>
2023-06-01 17:59:57 +0700

    display model details on admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,54 @@
 from django.contrib import admin
 from .models import Item, Category, Type, Transaction, Cart, Address, Variation
 
-admin.site.register(Item)
-admin.site.register(Category)
-admin.site.register(Type)
-admin.site.register(Transaction)
-admin.site.register(Cart)
-admin.site.register(Address)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+        'category',
+        'type',
+        'price',
+        'discounted_price',
+        'stock',
+        'sold_count'
+    ]
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'paid_at',
+        'shipping_address',
+    ]
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'item',
+        'variation',
+        'quantity',
+        'buy_price',
+    ]
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'label_name',
+        'shipping_address',
+        'shipping_notes',
+        'city',
+        'postal_code',
+        'default',
+    ]
+
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Variation)
+admin.site.register(Type)
+admin.site.register(Category)
+
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Address, AddressAdmin)
